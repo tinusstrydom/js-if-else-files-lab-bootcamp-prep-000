bb76040c425b90a8900b3c8c19a565195d4d5158
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-07 19:25:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,3 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
-if (name === "Joe"){
-  console.log("The name is correct, its "+name+" right!");
-}else if(name === "Susan"){
-  console.log("The name is correct, its "+name+" right!");
-}else{
-  console.log("Hi, not sure who you are !");
-}
